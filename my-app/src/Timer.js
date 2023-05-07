1741c63c4b4ea452f13b70418b9214fad45f4b3f
David Stepanov <davidstepanov@yahoo.com>
2023-05-07 17:46:05 -0400

    Added secondDate state. Needs further styling

--- my-app/src/Timer.js
+++ my-app/src/Timer.js
@@ -3,12 +3,15 @@ import React, { useState } from "react";
 function Timer() {
 
     const [inputtedDate, setInputtedDate] = useState('');
+    const [secondDate, setSecondDate] = useState('')
     const [intervals, setIntervals] = useState({});
+    const [isChecked, setIsChecked] = useState(true);
 
     function calculateIntervals() {
         let custom = new Date(inputtedDate);
+        let custom2 = new Date(secondDate);
         let now = Date.now();
-        let interval = now - custom;
+        let interval = isChecked ? now - custom : Math.abs(custom - custom2);
         let intervalSecs = interval / 1000;
         let intervalMins = intervalSecs / 60;
         let intervalHours = intervalMins / 60;
@@ -34,11 +37,18 @@ function Timer() {
         calculateIntervals();
     }
 
+    console.log(secondDate)
+
     return (
         <div style={{ marginTop: '100px' }}>
             <form onSubmit={submitted}>
                 <input placeholder="MM/DD/YYYY" onChange={(e) => setInputtedDate(e.target.value)}></input>
-                <button>Click me</button>
+                <input type="checkbox" onClick={() => setIsChecked(!isChecked)} checked={isChecked}></input>
+                <label htmlFor='checkbox'>Use today's date</label>
+                {!isChecked ?
+                    <input placeholder="MM/DD/YYYY" onChange={(e) => setSecondDate(e.target.value)}></input>
+                    : null}
+                <button>Calculate</button>
             </form>
             {Object.entries(intervals).map(([key, value]) => (
                 <p key={key}>{key}: {value}</p>
