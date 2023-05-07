1dcb641f1d204c4d2872a8256fbd5855549060e7
David Stepanov <davidstepanov@yahoo.com>
2023-05-07 17:27:40 -0400

    Age calculator finished

--- my-app/src/Timer.js
+++ my-app/src/Timer.js
@@ -1,21 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function Timer() {
 
-    let now = Date.now();
-    let custom = new Date('12/31/2001')
-    // console.log(now)
-    // console.log(custom.getTime())
-    console.log((now - custom.getTime()) / 1000 / 60 / 60 / 24 / 365)  //THIS SHOWS HOW MANY YEARS ITS BEEN SINCE CUSTOM DATE
-    // console.log(d.getTime() - 10000)
+    const [inputtedDate, setInputtedDate] = useState('');
+    const [intervals, setIntervals] = useState({});
 
+    function calculateIntervals() {
+        let custom = new Date(inputtedDate);
+        let now = Date.now();
+        let interval = now - custom;
+        let intervalSecs = interval / 1000;
+        let intervalMins = intervalSecs / 60;
+        let intervalHours = intervalMins / 60;
+        let intervalDays = intervalHours / 24;
+        let intervalWeeks = intervalDays / 7;
+        let intervalMonths = intervalDays / (365 / 12);
+        let intervalYears = intervalDays / 365;
+
+        setIntervals({
+            'Miliseconds': interval,
+            'Seconds': intervalSecs,
+            'Minutes': intervalMins,
+            'Hours': intervalHours,
+            'Days': intervalDays,
+            'Weeks': intervalWeeks,
+            'Months': intervalMonths,
+            'Years': intervalYears
+        });
+    }
+
+    function submitted(e) {
+        e.preventDefault();
+        calculateIntervals();
+    }
 
     return (
         <div style={{ marginTop: '100px' }}>
-            <input placeholder="month"></input>
-            <input placeholder="day"></input>
-            <input placeholder="year"></input>
-            <h1>Oops forgot to change placeholder</h1>
+            <form onSubmit={submitted}>
+                <input placeholder="MM/DD/YYYY" onChange={(e) => setInputtedDate(e.target.value)}></input>
+                <button>Click me</button>
+            </form>
+            {Object.entries(intervals).map(([key, value]) => (
+                <p key={key}>{key}: {value}</p>
+            ))}
         </div>
     )
 }
