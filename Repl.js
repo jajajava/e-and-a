eaaeb4e074b9c0f071ad26a77d421cb163aef008
David Stepanov <davidstepanov@yahoo.com>
2023-05-19 14:11:23 -0400

    Successful recurve function that calls the fib() function

--- Repl.js
+++ Repl.js
@@ -10,11 +10,22 @@
 
 
 function fib(first, second, end) {
-    let holder = [first, second]
+    let holder = [first, second] // If you wanted to, you could remove "first"/"second" parameters and replace the holder with [1, 1] to start out.
     for (let i = first; i <= end; i++) {
         holder.push(holder[holder.length - 1] + holder[holder.length - 2])  // This function is the fibonacci sequence, a very famous example of recursion in math
     }
     return holder
 }
 
-console.log(fib(1, 1, 1000))
+// console.log(fib(1, 1, 1000))
+
+function fibRecurve(n) {
+    if (n <= 0) {
+        console.log('Done!')
+    } else {                                // This function is the recursive function for the fib() function. It calls itself several times,
+        console.log(fib(1, 1, n + 1))       // which in turn executes fib until condition met (called n times)
+        return fibRecurve(n - 1)
+    }
+}
+
+console.log(fibRecurve(4))
