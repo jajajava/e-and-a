121fcdfc0ce0fdc7de899e690fcca295438764ea
David Stepanov <davidstepanov@yahoo.com>
2023-05-19 13:15:32 -0400

    Fibonacci's sequence algorithm

--- Repl.js
+++ Repl.js
@@ -1,9 +1,20 @@
-function solver(base, exp) {
-    let newBase = 1
-    for (let i = exp; i > 0; i--) {
-        newBase = base * newBase
+// function solver(base, exp) {
+//     let newBase = 1
+//     for (let i = exp; i > 0; i--) {
+//         newBase = base * newBase             // This function takes a base and exponent and does the math. Easier to just do "**", but this is just another option
+//     }
+//     return newBase
+// }
+
+// console.log(solver(3, 3))
+
+
+function fib(first, second, end) {
+    let holder = [first, second]
+    for (let i = first; i <= end; i++) {
+        holder.push(holder[holder.length - 1] + holder[holder.length - 2])  // This function is the fibonacci sequence, a very famous example of recursion in math
     }
-    return newBase
+    return holder
 }
 
-console.log(solver(3, 3))
+console.log(fib(1, 1, 1000))
