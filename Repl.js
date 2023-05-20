f41f99a10a5388335353ac187d0bb7afddc6356e
David Stepanov <davidstepanov@yahoo.com>
2023-05-20 00:25:02 -0400

    Notes for python basics

--- Repl.js
+++ Repl.js
@@ -1,7 +1,7 @@
 // function solver(base, exp) {
 //     let newBase = 1
 //     for (let i = exp; i > 0; i--) {
-//         newBase = base * newBase             // This function takes a base and exponent and does the math. Easier to just do "**", but this is just another option
+//         newBase = base * newBase             //* This function takes a base and exponent and does the math. Easier to just do "**", but this is just another option
 //     }
 //     return newBase
 // }
@@ -9,23 +9,24 @@
 // console.log(solver(3, 3))
 
 
-function fib(first, second, end) {
-    let holder = [first, second] // If you wanted to, you could remove "first"/"second" parameters and replace the holder with [1, 1] to start out.
-    for (let i = first; i <= end; i++) {
-        holder.push(holder[holder.length - 1] + holder[holder.length - 2])  // This function is the fibonacci sequence, a very famous example of recursion in math
-    }
-    return holder
-}
+// function fib(first, second, end) {
+//     let holder = [first, second]         //* If you wanted to, you could remove "first"/"second" parameters and replace the holder with [1, 1] to start out.
+//     for (let i = first; i <= end; i++) {
+//         holder.push(holder[holder.length - 1] + holder[holder.length - 2])  //* This function is the fibonacci sequence, a very famous example of recursion in math
+//     }
+//     return holder
+// }
 
 // console.log(fib(1, 1, 1000))
 
-function fibRecurve(n) {
-    if (n <= 0) {
-        console.log('Done!')
-    } else {                                // This function is the recursive function for the fib() function. It calls itself several times,
-        console.log(fib(1, 1, n + 1))       // which in turn executes fib until condition met (called n times)
-        return fibRecurve(n - 1)
-    }
-}
+// function fibRecurve(n) {
+//     if (n <= 0) {
+//         console.log('Done!')
+//     } else {                                //* This function is the recursive function for the fib() function. It calls itself several times,
+//         console.log(fib(1, 1, n + 1))       //* which in turn executes fib until condition met (called n times)
+//         return fibRecurve(n - 1)
+//     }
+// }
 
-console.log(fibRecurve(4))
+// console.log(fibRecurve(4))
+
---
+++ python_notes.py
@@ -0,0 +1,45 @@
+# Note: I am learning Python now, so please don't judge if it's weird code!
+
+# * Variable assignment: No let/const/var keywords; snake_case; just like Ruby
+# // pet_mood = 'hungry'
+# // pet_name = 'Jim'
+
+
+# * if/ elif/ else instead of if/elseif/else. Uses : and indentation to indicate scope in code, not curly braces (INDENT NEEDS TO BE 4 SPACES)
+# //  if pet_mood == 'hungry':
+# //    print("Feed the beast")
+# //  elif pet_mood == 'angry':
+# //    print("Time to get eaten")
+# //  else:
+# //    print("Jim is straight up chilling")
+
+# * Ternary operator in Python vs JavaScript
+# // print("This code if true") if (condition met) else print("this code if false")  ==> Python
+# // condition met ? console.log("This code if true") : console.log("this code if false") ==> JavaScript
+
+# * Function declaration: snake_case; def keyword instead of function (like ruby); unlike ruby, needs (): and note the use of a colon; function needs to be invoked to work
+# // def say_hello():
+# //    print("Haiii :>")
+
+
+# // say_hello()
+
+# * String interpolation in python vs JavaScript
+# // f"My name is {name}"  ==> Python
+# // `My name is ${name}` ==> JavaScript
+
+# * Default parameter here; can call function without passing argument
+
+
+# // def greet(name="david"):
+# //    print(f"hello, {name}")
+
+
+# // greet()  ==> "hello, david"
+
+
+# // def greet(name):
+# //    print(f"hello, {name}")
+
+
+# // greet("David")  ==> "hello, David"
