eff30960a3c6b582058c4ee5d327337ffde72243
David Stepanov <davidstepanov@yahoo.com>
2023-06-04 16:19:48 -0400

    Figured out navbar styling issue

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Header() {
     return (
-        <header id='header'>
-
+        <header className='header'>
+            <h1 className="logo">Placeholder_title</h1>
+            <h2 className="login">Login</h2>
         </header>
     )
 }
