8039aef40f4e9aac72d052865de57795b43799ef
David Stepanov <davidstepanov@yahoo.com>
2023-06-08 13:24:19 -0400

    Reinstalled Rails to remove unnecessary files, created ruby_and_rails_notes.rb

---
+++ ruby_and_rails_notes.rb
@@ -0,0 +1,7 @@
+#* Similarly to how React takes JavaScript and changes the way you write with it, Rails replaces the need for raw Ruby code.
+
+#* We are going to use Rails as an API
+
+#* In order to make a minimal API using Rails, you can type the following command:
+#// rails new app_name --api --minimal
+
