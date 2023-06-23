bec0ef9a386fd6e5626eda510c1c9c7af3eb6383
David Stepanov <davidstepanov@yahoo.com>
2023-06-23 00:09:04 -0400

    Added pin logic

--- my-app/frontend/src/Components/Login.js
+++ my-app/frontend/src/Components/Login.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { TiDelete } from "react-icons/ti";
 
 function Login() {
+
+    const [pin, setPin] = useState('')
+
+    function handlePin(e){
+        e.preventDefault()
+        setPin(pin + e.target.innerText)
+        if (pin.length === 4){
+            setPin(pin)
+        }
+    }
+    console.log(pin)
 
     return (
         <div className="background" style={{ backgroundColor: 'rgba(255, 166, 0, 0.884)' }}>
             <h1 className="loginLogo">🥐 toasty </h1>
             <h1 className="loginGreeting">Welcome!</h1>
             <form className='loginForm'>
+            <div id="pinInputContainer">
+                <input id="loginInput" type="password" maxlength="4"></input>
+                <TiDelete id="TiDelete"/>
+            </div>
             <table>
             <tr>
-                <td><button className="pinButton">1</button></td>
-                <td><button className="pinButton">2</button></td>
-                <td><button className="pinButton">3</button></td>
+                <td><button className="pinButton" onClick={handlePin}>1</button></td>
+                <td><button className="pinButton" onClick={handlePin}>2</button></td>
+                <td><button className="pinButton" onClick={handlePin}>3</button></td>
             </tr>
             <tr>
-                <td><button className="pinButton">4</button></td>
-                <td><button className="pinButton">5</button></td>
-                <td><button className="pinButton">6</button></td>
+                <td><button className="pinButton" onClick={handlePin}>4</button></td>
+                <td><button className="pinButton" onClick={handlePin}>5</button></td>
+                <td><button className="pinButton" onClick={handlePin}>6</button></td>
             </tr>
             <tr>
-                <td><button className="pinButton">7</button></td>
-                <td><button className="pinButton">8</button></td>
-                <td><button className="pinButton">9</button></td>
+                <td><button className="pinButton" onClick={handlePin}>7</button></td>
+                <td><button className="pinButton" onClick={handlePin}>8</button></td>
+                <td><button className="pinButton" onClick={handlePin}>9</button></td>
             </tr>
             </table>
             <div style={{alignSelf: 'center'}}>
