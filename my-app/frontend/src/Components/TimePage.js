10a34fc2e9b2fc5b9384cf4dc8a0d5e8ee61dc54
David Stepanov <davidstepanov@yahoo.com>
2023-07-15 00:11:30 -0400

    Fixed clocking issues!

--- my-app/frontend/src/Components/TimePage.js
+++ my-app/frontend/src/Components/TimePage.js
@@ -1,39 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "./App";
 
-function TimePage(){
-    let user = useContext(Context)
-    const token = localStorage.getItem('jwt')
-    let date = new Date()
+function TimePage() {
+    let user = useContext(Context);
+    const [clockedIn, setClockedIn] = useState(user.is_clocked_in);
+    let date = new Date();
 
-    function handleClocking(e){
-        e.preventDefault()
-        fetch(`http://127.0.0.1:3001/users/${user.id}`, {
+    let variableBody;
+    if (clockedIn === false) {
+    variableBody = {
+        time_in: date.toISOString(),
+        is_clocked_in: true
+    };
+    } else {
+    variableBody = {
+        time_in: null,
+        is_clocked_in: false
+    };
+    }
+
+    function handleClocking(e) {
+    e.preventDefault();
+    fetch(`http://127.0.0.1:3001/users/${user.id}`, {
         method: 'PATCH',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
         },
-        body: JSON.stringify({
-            time_in: date.toISOString()
-        })
-        })
+        body: JSON.stringify(variableBody)
+    })
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+        setClockedIn(!clockedIn);
+        console.log(data);
         })
         .catch(error => {
-            console.error(error);
+        console.error(error);
         });
     }
 
     return (
-        <div>
-            <form onSubmit={handleClocking}>
-                <button>{user.time_in !== null ? "Clock out" : "Clock in"}</button>
-            </form>
-        </div>
-    )
-}
+    <div>
+        <form onSubmit={handleClocking}>
+        <button>{clockedIn === true ? 'Clock out' : 'Clock in'}</button>
+        </form>
+    </div>
+    );
+  }
 
 export default TimePage
