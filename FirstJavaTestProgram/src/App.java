c97761b2eee78e4f0045d8c9b8fb0afdf1a2945d
David <davidstepanov@yahoo.com>
2023-08-16 12:33:44 -0400

    Java modal window

--- FirstJavaTestProgram/src/App.java
+++ FirstJavaTestProgram/src/App.java
@@ -1,9 +1,13 @@
-public class App {
-    // Following tutorial provided in class
-    public static void main(String args[]){
-        System.out.println("Welcome to Java");
-    }
-}
+// 8/15/23 - First program (following tutorial provided in class)
+
+// public class App {
+//     public static void main(String args[]){
+//         JOptionPane.showMessageDialog(null, "test", "header", JOptionPane.CANCEL_OPTION);
+//         // System.out.println("Welcome to Java");
+//     }
+// }
+
+// Note how the indentation looks; You nest it like that to show a heirarchal structure, for better readability.
 
 /*
     IMPORTANT JAVA NOTES:
@@ -12,3 +16,14 @@ public class App {
     When you run the command javac, the compiler checks for syntax errors. If the code is clean, it creates (or updates) a file called FileName.class
     Running java FileName is actually the JVM reading the compiled .class file (known as compiled bytecode), not the .java file (known as source code). 
  */
+
+
+// 8/16/23 - This program makes a modal window/message come up. Note the import of the java Library before the App class.
+
+// import javax.swing.JOptionPane;
+
+// public class App {
+//     public static void main(String args[]){
+//         JOptionPane.showMessageDialog(null, "test", "header", JOptionPane.CANCEL_OPTION);
+//     }
+// }
