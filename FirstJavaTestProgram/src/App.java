fa014bb21e450b5a8eb0ab0b3589f0440be12569
David <davidstepanov@yahoo.com>
2023-08-15 21:53:11 -0400

    Saving first java program

---
+++ FirstJavaTestProgram/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    // Following tutorial provided in class
+    public static void main(String args[]){
+        System.out.println("Welcome to Java");
+    }
+}
+
+/*
+    IMPORTANT JAVA NOTES:
+
+    Java consists of the compiler, called "Javac", and the interpreter, called the "Java Virtual Machine", or JVM. 
+    When you run the command javac, the compiler checks for syntax errors. If the code is clean, it creates (or updates) a file called FileName.class
+    Running java FileName is actually the JVM reading the compiled .class file (known as compiled bytecode), not the .java file (known as source code). 
+ */
