0f5f4157f47e7a977c3bbad582e06b63e1dd7af8
David Stepanov <davidstepanov@yahoo.com>
2023-09-23 18:34:15 -0400

    Subcategory state created

--- my-app/frontend/src/Components/KitchenDisplay.js
+++ my-app/frontend/src/Components/KitchenDisplay.js
@@ -4,6 +4,8 @@ import Header from "./Header";
 // This component is the home page, which is conditionally set depending on the user's state
 function KitchenDisplay() {
 
+    // Need to make a useEffect that fetches the current orders
+
     return (
         <div className="">
             <Header />
--- my-app/frontend/src/Components/OrderPage.js
+++ my-app/frontend/src/Components/OrderPage.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import Header from "./Header";
 
+//! Because of the subcategory, you'll need to modify the backend (seed data, database)
 // This component is for the page used to create new orders
 function OrderPage({toHomepage}){
 
     const [selectedMainCategory, setSelectedMainCategory] = useState("food");
-    // const [selectedSubCategory, setSelectedSubCategory] = useState("mains") // Make this conditional so that if the main is food, it's "mains", else "drinks"
+    const [selectedSubCategory, setSelectedSubCategory] = useState("mains");
 
     return(
         <div>
             <Header toHomepage={toHomepage}/>
             <div className="orderMainCategory">
                     {/* Food has "mains", "sauces", and "sides" */}
-                    <button onClick={()=>{setSelectedMainCategory("food")}}>Food</button> 
+                    <button onClick={()=>{setSelectedMainCategory("food"); setSelectedSubCategory("mains")}}>Food</button> 
                     {/* Drink has "drinks" */}
-                    <button onClick={()=> {setSelectedMainCategory("drink")}}>Drink</button>
+                    <button onClick={()=> {setSelectedMainCategory("drink"); setSelectedSubCategory("non-alc")}}>Drink</button>
                     {selectedMainCategory === "food" ? 
                         <div>
                             <div className="orderSubCategory"></div>
