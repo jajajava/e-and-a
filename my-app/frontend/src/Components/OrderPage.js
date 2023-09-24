6617b66ffcdb4bdeba37b1e2ec2f2634f92c23b2
David Stepanov <davidstepanov@yahoo.com>
2023-09-24 17:43:37 -0400

    Created orderArray to take the order!

--- my-app/frontend/src/Components/OrderPage.js
+++ my-app/frontend/src/Components/OrderPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 
-//! Because of the subcategory, you'll need to modify the backend (seed data, database)
+//! Modify the seed data after whole menu is done to create all items in DB
 // This component is for the page used to create new orders
 function OrderPage({toHomepage}){
 
@@ -10,40 +10,41 @@ function OrderPage({toHomepage}){
     // I'm not yet sure if subcategory2 needs to exist for foods, but it does for drink types.
     const [selectedSubCategory2, setSelectedSubCategory2] = useState("soft drinks");
     const [selectedSubCategory2Array, setSelectedSubCategory2Array] = useState([])
+    const [orderArray, setOrderArray] = useState([])
 
     // <button className="orderSubCategory"></button>
 
-    useEffect(()=> {
+    useEffect(()=> { //! Add onClick to all buttons that adds it to the array that is the order- json body for POST call
         let tempArray = []
         if (selectedSubCategory2 === "soft drinks"){
             tempArray = [
-            <button className="orderSubCategory">Coca Cola</button>,
-            <button className="orderSubCategory">Diet Coke</button>,
-            <button className="orderSubCategory">Sprite</button>,
-            <button className="orderSubCategory">Pepsi</button>,
-            <button className="orderSubCategory">Fanta</button>,
-            <button className="orderSubCategory">Root Beer</button>,
-            <button className="orderSubCategory">Ginger Ale</button>
+            <button onClick={()=> setOrderArray([...orderArray, "Coca Cola"])} className="orderSubCategory">Coca Cola</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Diet Coke"])} className="orderSubCategory">Diet Coke</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Sprite"])} className="orderSubCategory">Sprite</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Pepsi"])} className="orderSubCategory">Pepsi</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Fanta"])} className="orderSubCategory">Fanta</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Root Beer"])} className="orderSubCategory">Root Beer</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Ginger Ale"])} className="orderSubCategory">Ginger Ale</button>
             ]
         } else if (selectedSubCategory2 === "lemonades"){
             tempArray = [
-            <button className="orderSubCategory">Classic</button>,
-            <button className="orderSubCategory">Pink</button>,
-            <button className="orderSubCategory">Blueberry</button>
+            <button onClick={()=> setOrderArray([...orderArray, "Classic Lemonade"])} className="orderSubCategory">Classic</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Pink Lemonade"])} className="orderSubCategory">Pink</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Raspberry Lemonade"])} className="orderSubCategory">Raspberry</button>
             ]
         } else if (selectedSubCategory2 === "frozen"){
             tempArray = [
-            <button className="orderSubCategory">Strawberry limeade</button>,
-            <button className="orderSubCategory">Blueberry limeade</button>,
-            <button className="orderSubCategory">Cherry limeade</button>,
-            <button className="orderSubCategory">Peach limeade</button>,
-            <button className="orderSubCategory">Mango limeade</button>
+            <button onClick={()=> setOrderArray([...orderArray, "Frozen Strawberry"])} className="orderSubCategory">Strawberry limeade</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Frozen Blueberry"])} className="orderSubCategory">Blueberry limeade</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Frozen Cherry"])} className="orderSubCategory">Cherry limeade</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Frozen Peach"])} className="orderSubCategory">Peach limeade</button>,
+            <button onClick={()=> setOrderArray([...orderArray, "Frozen Mango"])} className="orderSubCategory">Mango limeade</button>
             ]
         }
         setSelectedSubCategory2Array(tempArray)
-    }, [selectedSubCategory2])
+    }, [selectedSubCategory2, orderArray])
 
-
+    console.log(orderArray)
     return(
         <div>
             <Header toHomepage={toHomepage}/>
