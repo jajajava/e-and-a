95aaaa52376840d72af7765103a228d0ea500cf3
David <davidstepanov@yahoo.com>
2023-12-12 19:29:54 -0500

    Huge overhaul of OrderPage component; displays fetched data conditionally

--- my-app/frontend/src/Components/OrderPage.js
+++ my-app/frontend/src/Components/OrderPage.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 
-//! Modify the seed data after whole menu is done to create all items in DB
 // This component is for the page used to create new orders
 function OrderPage({toHomepage}){
 
     const [selectedMainCategory, setSelectedMainCategory] = useState("food");
-    const [selectedSubCategory, setSelectedSubCategory] = useState("mains");
+    const [selectedSubcategory, setSelectedSubcategory] = useState("cocktails")
     //I'm not yet sure if subcategory2 needs to exist for foods, but it does for drink types.
-    const [selectedSubCategory2, setSelectedSubCategory2] = useState("soft drinks");
-    const [selectedSubCategory2Array, setSelectedSubCategory2Array] = useState([])
+    const [alcoholSelected, setAlcoholSelected] = useState(true)
     const [orderArray, setOrderArray] = useState([])
     const [foodsArray, setFoodsArray] = useState([])
     const [drinksArray, setDrinksArray] = useState([])
 
-
     //! orderArray to be used as JSON body for POST call. 
-    //! You should put all the arrays like "tempArray" (all menu organizing arrays) into a separate file and import it
+    //! Need to fix styling on buttons
+    // Fetches menu
     useEffect(()=> {
         fetch("http://127.0.0.1:3001/foods", {
             method: "GET",
@@ -31,74 +29,57 @@ function OrderPage({toHomepage}){
             setDrinksArray(prevDrinksArray => [...prevDrinksArray, ...data.filter(x => x.category === "drinks")])})
     }, [])
 
-    console.log(foodsArray)
-    console.log(drinksArray)
+    // Keeps the console clean, only logs orderArray when there's a state change
+    useEffect(()=> {
+        console.log(orderArray)
+    }, [orderArray])
 
-
-    // //! MAKE THE USE EFFECT FETCH DATA FROM BACKEND, THEN DO .MAP() TO CREATE THE FOLLOWING BUTTONS FOR EACH CATEGORY
-    // useEffect(()=> {
-    //     let tempArray = []
-    //     if (selectedSubCategory2 === "soft drinks"){
-    //         tempArray = [
-    //         <button onClick={()=> setOrderArray([...orderArray, "Coca Cola"])} className="orderSubCategory">Coca Cola</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Diet Coke"])} className="orderSubCategory">Diet Coke</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Sprite"])} className="orderSubCategory">Sprite</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Pepsi"])} className="orderSubCategory">Pepsi</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Fanta"])} className="orderSubCategory">Fanta</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Root Beer"])} className="orderSubCategory">Root Beer</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Ginger Ale"])} className="orderSubCategory">Ginger Ale</button>
-    //         ]
-    //     } else if (selectedSubCategory2 === "lemonades"){
-    //         tempArray = [
-    //         <button onClick={()=> setOrderArray([...orderArray, "Classic Lemonade"])} className="orderSubCategory">Classic</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Pink Lemonade"])} className="orderSubCategory">Pink</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Raspberry Lemonade"])} className="orderSubCategory">Raspberry</button>
-    //         ]
-    //     } else if (selectedSubCategory2 === "frozen"){
-    //         tempArray = [
-    //         <button onClick={()=> setOrderArray([...orderArray, "Frozen Strawberry"])} className="orderSubCategory">Strawberry limeade</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Frozen Blueberry"])} className="orderSubCategory">Blueberry limeade</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Frozen Cherry"])} className="orderSubCategory">Cherry limeade</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Frozen Peach"])} className="orderSubCategory">Peach limeade</button>,
-    //         <button onClick={()=> setOrderArray([...orderArray, "Frozen Mango"])} className="orderSubCategory">Mango limeade</button>
-    //         ]
-    //     }
-    //     setSelectedSubCategory2Array(tempArray)
-    // }, [selectedSubCategory2, orderArray])
-
-    console.log(orderArray)
     return(
         <div>
             <Header toHomepage={toHomepage}/>
             <div className="orderMainCategory">
-                    {/* Food has "mains", "sauces", and "sides" */}
-                    <button onClick={()=>{setSelectedMainCategory("food"); setSelectedSubCategory("mains")}}>Food</button> 
-                    {/* Drink has "drinks" */}
-                    <button onClick={()=> {setSelectedMainCategory("drink"); setSelectedSubCategory("non-alc")}}>Drink</button>
-                    {selectedMainCategory === "food" ? 
-                        <div>
-                            
-                        </div> 
-                    :   <div>
-                            <button className="orderSubCategory" onClick={() => {setSelectedSubCategory("non-alc"); setSelectedSubCategory2("soft drinks")}}>Non-alc</button>
-                            <button className="orderSubCategory" onClick={() => {setSelectedSubCategory("alcohol"); setSelectedSubCategory2("beers")}}>Alcohol</button>
-                            {selectedSubCategory === "non-alc" ?
+                <button onClick={() => setSelectedMainCategory("food")}>Food</button>
+                <button onClick={() => setSelectedMainCategory("drinks")}>Drinks</button>
+                {selectedMainCategory === "food" ? 
+                    <div>
+                        {foodsArray.map((x) => <button key={x.id} onClick={() => setOrderArray([...orderArray, x])}>{x.name}</button>)} 
+                    </div>
+                    : <div>
+                        <button onClick={() => setAlcoholSelected(true)}>Alcohol</button>
+                        <button onClick={() => setAlcoholSelected(false)}>Non-Alc</button>
+                        {alcoholSelected === true ? 
                             <div>
-                                <div>
-                                    <button id="orderSubCategory2" className="orderSubCategory" onClick={() => {setSelectedSubCategory2("soft drinks");}}>Soft drinks</button>
-                                    <button id="orderSubCategory2" className="orderSubCategory" onClick={() => {setSelectedSubCategory2("lemonades");}}>Lemonades</button>
-                                    <button id="orderSubCategory2" className="orderSubCategory" onClick={() => {setSelectedSubCategory2("frozen");}}>Frozen Drinks</button>
-                                </div>
-                                {selectedSubCategory2Array}
-                            </div>
-                            :   <div>
-                                    <button className="orderSubCategory">Beers</button>
-                                    <button className="orderSubCategory">Wines</button>
-                                    <button className="orderSubCategory">Cocktails</button>
-                                    <button className="orderSubCategory">Spirits</button>
-                                </div>
+                                <button onClick={()=> setSelectedSubcategory("cocktails")}>Cocktails</button>
+                                <button onClick={()=> setSelectedSubcategory("wines")}>Wines</button>
+                                <button onClick={()=> setSelectedSubcategory("spirits")}>Spirits</button>
+                                <button onClick={()=> setSelectedSubcategory("beers")}>Beers</button>
+                                {/* You will need to replace .subcategory2 with category in the backend */}
+                                {selectedSubcategory === "cocktails" ?
+                                <div> {drinksArray.filter((x) => (x.is_alc === true && x.subcategory2 === "cocktails")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : selectedSubcategory === "wines" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === true && x.subcategory2 === "wines")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : selectedSubcategory === "spirits" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === true && x.subcategory2 === "spirits")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : selectedSubcategory === "beers" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === true && x.subcategory2 === "beers")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : null
                             }
-                        </div>}
+                            </div> 
+                            : <div>
+                                <button onClick={()=> setSelectedSubcategory("soft drinks")}>Soft drinks</button>
+                                <button onClick={()=> setSelectedSubcategory("lemonades")}>Lemonades</button>
+                                <button onClick={()=> setSelectedSubcategory("frozen")}>Frozen</button>
+                                {/* Loads options for selected category (non alcoholic drinks) */}
+                                {selectedSubcategory === "soft drinks" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === false && x.subcategory2 === "soft drinks")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : selectedSubcategory === "lemonades" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === false && x.subcategory2 === "lemonades")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : selectedSubcategory === "frozen" ? 
+                                <div> {drinksArray.filter((x) => (x.is_alc === false && x.subcategory2 === "frozen")).map((y) => <button key={y.id} onClick={() => setOrderArray([...orderArray, y])}>{y.name}</button>)} </div>
+                                : null
+                                }
+                            </div>}
+                </div>}
             </div>
         </div>
     )
