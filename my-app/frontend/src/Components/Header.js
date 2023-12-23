51c8448ca923b0978ae159aa107ef2f62108135a
David <davidstepanov@yahoo.com>
2023-12-23 17:28:47 -0500

    Functionality and minor CSS update to show Tab in Header component

--- my-app/frontend/src/Components/Header.js
+++ my-app/frontend/src/Components/Header.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 // This component is the header; it should be the same on all pages, and only shown when the user is signed in.
 function Header() {
     const {toHomepage, user} = useContext(Context)
-    const [decision, setDecision] = useState("Make Order")
+    const [decision, setDecision] = useState("New Order")
 
     let navigate = useNavigate()
 
@@ -19,14 +19,14 @@ function Header() {
 
     function decideTabsOrNot(){
         if (window.location.href.endsWith("/tabs")){
-            setDecision("Make Order")
+            setDecision("New Order")
         } else {
             setDecision("See Tabs")
         }
     }
 
     function handleConditionalRouting(){
-        if (decision === "Make Order"){
+        if (decision === "New Order"){
             navigate('/')
         } else {
             navigate('/tabs')
@@ -46,8 +46,7 @@ function Header() {
         <header className='header'>
             <h1 className="logo" onClick={newOrder}>🥐 toasty </h1>
             <h1 className="greeting">Welcome, {user.name}.</h1>
-            <div className="nav-bar">
-                {/* If current page === /tabs, it's "make order", else /tabs */}
+            <div id={decision === "New Order" ? "headerToHome" : "headerToTabs"} className="nav-bar">
                 <h3 className="nav-link" onClick={handleConditionalRouting}>{decision}</h3>
                 <h3 className="nav-link" onClick={handleClocking}>Time Clock</h3>
                 <h3 className="nav-link" onClick={toKitchenDisplay}>Kitchen display</h3>
