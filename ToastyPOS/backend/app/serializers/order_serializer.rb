64d1a5eab7a3c65789ffa1a4de2d779095e9294f
David <davidstepanov@yahoo.com>
2023-12-26 19:16:36 -0500

    First version of KitchenCard finished

--- ToastyPOS/backend/app/serializers/order_serializer.rb
+++ ToastyPOS/backend/app/serializers/order_serializer.rb
@@ -2,4 +2,13 @@ class OrderSerializer < ActiveModel::Serializer
     attributes :id, :total, :tab_id, :is_complete, :created_at, :updated_at
     has_many :foods, through: :order_items
     belongs_to :tab, serializer: OrderTabSerializer
+    attribute :items, key: :order_items
+
+    def items
+        object.order_items.map do |order_item|{
+            id: order_item.id,
+            food_id: order_item.food_id,
+            quantity: order_item.quantity
+        } end
+    end
 end
--- ToastyPOS/frontend/src/Components/KitchenCard.js
+++ ToastyPOS/frontend/src/Components/KitchenCard.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function KitchenCard({order}){
+    const [loadedOrders, setLoadedOrders] = useState([])
+
+    // CALL THIS IN A USE EFFECT
+    useEffect(()=> {
+        orderLoad()
+    }, [])
+
+    function orderLoad() {
+        const newOrders = order.order_items.map((item, index) => (
+            <h4 key={index}>{order.foods[index].name} - {item.quantity}</h4>
+        ));
+        setLoadedOrders(newOrders);
+    }
+    
     return (
         <div>
-            {order.id}
-            
+            <h3>Order #{order.id}:</h3>
+            <h4>{order.order_items.length > 0 ? loadedOrders : null}</h4>
         </div>
     )
 }
