13b78d4fe3f0b33b07f15ee5e10443b571dec29e
David <davidstepanov@yahoo.com>
2025-01-20 01:25:17 -0500

    More dataframe practice

--- python/test_code.py
+++ python/test_code.py
@@ -395,7 +395,7 @@
 # from pathlib import Path
 # import datetime as dt
 # import csv
-# import polars as pl
+import polars as pl
 # import geopandas as gpd
 # from lonboard import Map, ScatterplotLayer
 
@@ -418,7 +418,7 @@
 # df = pl.DataFrame(
 #     {
 #         "Name": ["Ben", "James", "Sarah", "Felicia"],
-#         "Weight": [57.9, 72.5, 53.6, 83.1], # (kg)
+#         "Weight": [12, 57.9, 72.5, 53.6], # (kg)
 #         "Height": [1.56, 1.77, 1.65, 1.75], # (m)
 #     }
 # )
@@ -447,6 +447,17 @@
 # print(read.describe())
 
 # import polars as pl
+
+# df = pl.DataFrame(
+#     {
+#         "Name": ["Ben", "James", "Sarah", "Felicia"],
+#         "Weight": ["Steve", 57.9, 72.5, 53.6], # (kg)
+#         "Height": [1.56, 1.77, 1.65, 1.75], # (m)
+#     }, strict=False  <== Allows you to mix data types
+# )
+
+# pl.Config.set_tbl_rows(len(df))
+# print(df)
 
 # read = pl.read_csv("./world_fires_7_day.csv", try_parse_dates=True)
 # pl.Config.set_tbl_cols(len(read))
